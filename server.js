1a084181494817d8e45460cac51c1bfb65f62169
0x0 <ghstoreinvesting@gmail.com>
2017-12-17 15:52:52 +0800

    finish off tutorial

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let app = express();
 let mongoose = require('mongoose');
 let morgan = require('morgan');
 let bodyParser = require('body-parser');
-let port = 8080;
+let port = 8000;
 let book = require('./app/routes/book');
 let config = require('config'); //we load the db location from the JSON files
 require('dotenv').config() // Allow us to use environment variables to hide secrets/api keys etc
